7938e3414bb2d14491441ab18c5fa9737f685698
Tomasz Łucarz <tomasz.lucarz@gmail.com>
2026-01-04 17:11:35 +0100

    Code refactoring

---  tests/pulpit.spec.ts
+++  tests/pulpit.spec.ts
@@ -4,22 +4,32 @@ test.describe('Pulpit tests', () => {
 
     test('quick payment with correct data', async ({ page }) => {
 
-        await page.goto('https://demo-bank.vercel.app/');
+        // Arrange
+        const url = 'https://demo-bank.vercel.app/';
+        const userId = 'testlogin';
+        const userPassword = 'test1234';
+        const expectedUsername = 'Jan Demobankowy';
+        
+        const transferReceiver = 'Chuck Demobankowy';
+        const transferAmount = '150';
+        const transferTitle = 'Zwrot środków';
 
-        await page.getByTestId('login-input').fill('aaaaaaaa');
-        await page.getByTestId('password-input').fill('test1234');
+        // Act
+        await page.goto(url);
+        await page.getByTestId('login-input').fill(userId);
+        await page.getByTestId('password-input').fill(userPassword);
         await page.getByTestId('login-button').click();
-
         await page.waitForLoadState('networkidle'); // needed to load everything properly
 
-        await page.locator('#widget_1_transfer_receiver').selectOption({ label: 'Chuck Demobankowy' });
-        await page.locator('#widget_1_transfer_amount').fill('150');
-        await page.locator('#widget_1_transfer_title').fill('Zwrot środków');
+        await page.locator('#widget_1_transfer_receiver').selectOption({ label: transferReceiver });
+        await page.locator('#widget_1_transfer_amount').fill(transferAmount);
+        await page.locator('#widget_1_transfer_title').fill(transferTitle);
 
         await page.locator('#execute_btn').click(); // await page.getByRole('button', { name: 'wykonaj' }).click();
         await page.getByTestId('close-button').click();
 
-        await expect(page.locator('#show_messages')).toHaveText('Przelew wykonany! Chuck Demobankowy - 150,00PLN - Zwrot środków');
+        // Assert
+        await expect(page.locator('#show_messages')).toHaveText(`Przelew wykonany! ${transferReceiver} - ${transferAmount},00PLN - ${transferTitle}`);
     });
 
     test('successful mobile top-up', async ({ page }) => {
