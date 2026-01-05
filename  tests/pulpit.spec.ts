7370ac78217f3c2efa0c5bc488d54ce891422bbb
Tomasz Łucarz <tomasz.lucarz@gmail.com>
2026-01-05 21:35:50 +0100

    test correct balance after successful mobile top-up

---  tests/pulpit.spec.ts
+++  tests/pulpit.spec.ts
@@ -50,4 +50,22 @@ test.describe('Pulpit tests', () => {
     // Assert
     await expect(page.locator('#show_messages')).toHaveText(expectedMessage);
   });
+
+  test('correct balance after successful mobile top-up', async ({ page }) => {
+    // Arrange
+    const topupReceiver = '504 xxx xxx';
+    const topupAmount = '100';
+    const initialBalance = await page.locator('#money_value').innerText();
+    const expectedBalance = Number(initialBalance) - Number(topupAmount);
+
+    // Act
+    await page.locator('#widget_1_topup_receiver').selectOption(topupReceiver);
+    await page.locator('#widget_1_topup_amount').selectOption(topupAmount);
+    await page.locator('#uniform-widget_1_topup_agreement span').click();
+    await page.getByRole('button', { name: 'doładuj telefon' }).click();
+    await page.getByTestId('close-button').click();
+
+    // Assert
+    await expect(page.locator('#money_value')).toHaveText(`${expectedBalance}`);
+  });
 });
